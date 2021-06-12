674cf19d1268264c65f7b1e50db442de716d436f
Juan Esteban <juanestban000@gmail.com>
2021-06-12 00:10:11 -0500

    initializer api

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    yoda: 0,
+    'multiline-ternary': 0,
+    'no-unused-vars': 0,
+    'no-return-assign': 0,
+    'no-unused-expressions': 0,
+    'no-undef': 0,
+    'space-before-function-paren': 0,
+  },
+}
